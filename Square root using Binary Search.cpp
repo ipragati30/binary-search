44467eb5ce6be8df23ceeccb44aeaab93474d008
Pragati Singh <156079306+ipragati30@users.noreply.github.com>
2024-12-13 13:54:48 +0530

    Square root using Binary Search.cpp

--- Square root using Binary Search.cpp
+++ Square root using Binary Search.cpp
@@ -1,1 +1,20 @@
-
+int floorSqrt(int n) {
+        // Your code goes here
+        int low=1;
+        int high =n;
+         int mid;           
+        while(low<=high){
+            int mid =low+(high-low)/2;
+            if(mid==sqrt(n)){
+                return mid;
+            }
+            else if (mid>sqrt(n)){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            
+        }
+        return high;
+    }
